a73d6061c923ea3f7279433370dd12537d5fb14e
ljy0221 <jaeyeong221@gmail.com>
2024-03-18 11:00:50 +0900

    week3 / printf, scanf 실습

---
+++ week2/helloWorld.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    //printf("hello world!\n");
+
+    //week2 riview
+    printf("안녕하세요?\n");
+    printf("프로그램 올바르게 실행되네요\n");
+
+    return 0;
+}
---
+++ week3/add1.c
@@ -0,0 +1,18 @@
+/* 두개의 숫자의 합을 계산하는 프로그램*/
+#include <stdio.h>
+
+
+int main(){
+    int x;
+    int y;
+    int sum;
+
+    x = 100;
+    y = 200;
+
+    sum = x + y;
+    
+    printf("두수의 합: %d", sum);
+
+    return 0;
+}
